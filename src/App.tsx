7c03dc49f6bee8e0e69b2570187f00c455ace5cc
Dhananjay-JSR <ajaysenday@gmail.com>
2023-12-03 02:07:47 +0530

    Defined Data Fetch Schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,55 @@
-
+import { useEffect, useState } from "react";
 
 function App() {
+  interface DataType {
+    id:string,
+    name:string,
+    emai:string,
+    role:string
+  }
+  const [fetchedData, setFetchedData] = useState<DataType[]|null>(null);
 
+
+
+
+// Data Fetching Login
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    (async () => {
+      try {
+        const fetchRes = await fetch(
+          "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json",
+          {
+            signal: signal,
+          }
+        );
+        const data = await fetchRes.json() as DataType[];
+        setFetchedData(() => data);
+      } catch (err) {
+        if (err instanceof DOMException) {
+          console.log("aborted");
+        }
+      }
+    })()
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  if (fetchedData == null) {
+    return (
+      <h3 className="font-semibold text-center mt-12">Loading.. Dashboard..</h3>
+    );
+  }
 
   return (
     <>
-       <h1 className="text-3xl bg-red-400 font-bold underline">
-      Hello world!
-    </h1>
+      <pre>
+        <code>{JSON.stringify(fetchedData, null, 2)}</code>
+      </pre>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
